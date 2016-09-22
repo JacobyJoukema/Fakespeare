e443a680d5e92e451ceb7c2da0d184ae001681d8
JacobyJoukema <jacoby.joukema@gmail.com>
2016-09-21 21:21:33 -0400

    Added Metadata to ignore files. Also worked on Main.java and Network.java

--- Workspace/Fakespeare/src/Main/Main.java
+++ Workspace/Fakespeare/src/Main/Main.java
@@ -6,9 +6,11 @@ public class Main
 {
 	public static void main (String [] args)
 	{
+		
+	}
+	public static void trainDouble (double input[], double ideal[])
+	{
 		Network neural = new Network ()
-		double input [] = {0.0,1.0};
-		double output [] = neural.compute(input);
 		for (double out: output)
 		{
 			System.out.println(out);
--- Workspace/Fakespeare/src/Neurons/Network.java
+++ Workspace/Fakespeare/src/Neurons/Network.java
@@ -1,6 +1,25 @@
 package Neurons;
 
-public class Network {
+public class Network
+{
+	private double globalError;
+	private int inputCnt;
+	private int hiddenCnt;
+	private int outputCnt;
+	private int neuronCnt;
+	private int weightCnt;
+	private int learnRate;
+	private double momentum;
+	private double errorDelta;
+	private double [] fire;
+	private double [] matrix;
+	private double [] error;
+	private double [] accMatrixDelta;
+	private double [] thresholds;
+	private double [] matrixDelta;
+	private double [] accThresholdDelta;
+	private double [] thresholdDelta;
+	
 	public Network (int inputCnt, int hiddenCnt, double learnRate, double momentum)
 	{
 		
