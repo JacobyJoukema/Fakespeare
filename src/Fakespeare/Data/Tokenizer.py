79f0092186090a5ab48903d9556cfdf16f0b1870
JacobyJoukema <joukemjw@mcmaster.ca>
2017-02-14 18:41:45 -0500

    Corrected Tokenizer

--- src/Fakespeare/Data/Tokenizer.py
+++ src/Fakespeare/Data/Tokenizer.py
@@ -1,7 +1,16 @@
 import nltk
 
+senStart = "SENTENCE_START"
+senEnd = "SENTENCE_END"
+
 f = open('Lines.txt', 'r')
 text = f.read()
+lines = text.split("--------------")
 
-tokenized = [nltk.word_tokenize(line) for line in text.split("--------------")]
+lines = [line.replace('\n', ' ') for line in lines]
+#print (lines)
+sentences = [nltk.sent_tokenize(line.lower()) for line in lines]
+sentences = ["%s %s %s" % (senStart, sentence, senEnd) for sentence in sentences]
+
+tokenized = [nltk.word_tokenize(sentence) for sentence in sentences]
 print (tokenized)
