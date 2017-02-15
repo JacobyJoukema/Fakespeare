d7a8d6f6b5a3d7eb8ccd72154d2c137d2a960d7f
JacobyJoukema <joukemjw@mcmaster.ca>
2017-02-15 14:36:59 -0500

    Completed Tokenizer

--- src/Fakespeare/sys/Tokenizer.py
+++ src/Fakespeare/sys/Tokenizer.py
@@ -1,9 +1,13 @@
 import nltk
+import numpy as np
+import itertools
 
 class Tokenizer:
     def __init__ (self):
         senStart = "SENTENCE_START"
         senEnd = "SENTENCE_END"
+        unknown = "UNKNOWN_TOKEN"
+        vocabSize = 15000
 
         f = open('Data/Lines.txt', 'r')
         text = f.read()
@@ -12,10 +16,20 @@ class Tokenizer:
         lines = [line.replace('\n', ' ') for line in lines]
         #print (lines)
         sentences = [nltk.sent_tokenize(line.lower()) for line in lines]
-        sentences = ["%s %s %s" % (senStart,sentence, senEnd) for sentence in sentences]
-
+        #['Doth u swag?']
+        sentences = ["%s %s %s" % (senStart,sentence[0], senEnd) for sentence in sentences]
+        #['SENTENCE_START', ['Doth', 'u', 'swag', '?'], 'SENTENCE_END']
         tokenized = [nltk.word_tokenize(sentence) for sentence in sentences]
-        print (tokenized)
+
+        #Ref
+        wordFreq = nltk.FreqDist(itertools.chain(*tokenized))
+        vocab = wordFreq.most_common(vocabSize-1)
+        indexToWord = [x[0] for x in vocab]
+        indexToWord.append(unknown)
+        wordToInd = dict([(w,i) for i,w in enumerate(indexToWord)])
+
+        for i, sent in enumerate(tokenized):
+            tokenized[i] = [w if w in wordToInd else unknown for w in sent]
 
         #Ref
         xTrain = np.asarray([[wordToInd[word] for word in sent[:-1]] for sent in tokenized])
