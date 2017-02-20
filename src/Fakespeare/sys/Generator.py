f6a20060619b6750e13108e8e68f31000ddc1158
JacobyJoukema <joukemjw@mcmaster.ca>
2017-02-20 17:32:53 -0500

    Small alterations to generator, includes more post parseing

--- src/Fakespeare/sys/Generator.py
+++ src/Fakespeare/sys/Generator.py
@@ -4,16 +4,28 @@ from Utils import *
 import numpy as np
 
 class Generator:
-    __init__ (self, fileName):
-        self.t = Tokenizer()
+    def __init__ (self, fileName):
+        t = Tokenizer()
         self.wordToInd = t.getWordToInd()
         self.indexToWord = t.getIndToWord()
         self.model = RNN(t.getVocabSize())
-        load(fileName, model)
+        load(fileName, self.model)
+
+    def postParse (self, sentence):
+        sentence = [v for v in sentence if not v == "[" or v == "]"]
+        out = str(sentence[0][0].upper()+sentence[0][1:])
+        for i in sentence[1:]:
+            if i == "," or i == "." or i ==":" or i == ";" or i=="?" or i=="!":
+                out += i
+            elif i=="i":
+                out+= " I"
+            else:
+                out += " " + i
+        return out
 
     def generateSentence (self):
-        newSent = [wordToInd["SENTENCE_START"]]
-        while not newSent[-1] == wordToInd["SENTENCE_END"]:
+        newSent = [self.wordToInd["SENTENCE_START"]]
+        while not newSent[-1] == self.wordToInd["SENTENCE_END"]:
             nextWordProbs = self.model.forwardPropagation(newSent)[0]
             sampled = self.wordToInd["UNKNOWN_TOKEN"]
             while sampled == self.wordToInd["UNKNOWN_TOKEN"]:
@@ -21,18 +33,12 @@ class Generator:
                 sampled = np.argmax(samples)
             newSent.append(sampled)
         sentence = [self.indexToWord[x] for x in newSent[1:-1]]
-        return (postParse(sentence))
+        #print (sentence)
+        return (self.postParse(sentence))
 
-    def postParse (self, sentence):
-        sentence[0].capitalize()
-        out = sentence[0]
-        for i in sentence[1:]:
-            if i == "," or i == "." or i ==":" or i == ";":
-                out += i
-            else:
-                out += " " + i
-        return out
-
+g = Generator("Data/FakespeareProg.npz")
 for i in range (10):
     print ("Generating Sentence " + str(i))
-    print (generateSentence(model, wordToInd, indexToWord))
+    sent = g.generateSentence()
+    print ("Sentence Length: " + str (len(sent)))
+    print (sent)
